e2aebf0e5f6ab0db4abdd7fe3e9500a92ce1617b
Oscar Nunez <noscar07@gmail.com>
2021-03-09 12:05:47 -0500

    a typo? really?

--- code.js
+++ code.js
@@ -5,24 +5,47 @@ let level = 0;
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 
+
+function activateTile(color){
+    const tile = document.querySelector(`[data-tile='${color}']`);
+    const sound = document.querySelector(`[data-sound='${color}']`);
+
+    tile.classList.add('activated');
+    sound.play();
+
+    setTimeout(() => {
+        title.classList.remove('activated');
+    }, 300);
+}
+
+function playRound(nextCompSequence) {
+    nextCompSequence.forEach((color, index) => {
+        setTimeout(() => {
+            activateTile(color);
+        }, (index + 1) * 600);
+    });
+}
+
 function nextStep() {
     const titles = ['red', 'green', 'blue', 'yellow'];
     const random = titles[Math.floor(Math.random() * titles.length)];
 
     return random;
-};
+}
 
 function nextRound() {
     level += 1;
 
     const nextCompSequence = [...compSequence];
     nextCompSequence.push(nextStep());
+    playRound(nextCompSequence);
 };
 
 function startGame() {
     startButton.classList.add('hidden');
     info.classList.remove('hidden');
     info.textContent = "I'll start first! hope you can remember."
+    nextRound();
 };
 
 startButton.addEventListener('click', startGame);
